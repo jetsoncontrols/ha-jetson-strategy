2196791587abae5c2ef6e954da78e1ac28d2300d
sshanafelt <sshanafelt@jetsonsys.com>
2025-07-30 15:18:32 -0400

    clean up old ref

--- src/jetson-strategy.ts
+++ src/jetson-strategy.ts
@@ -83,7 +83,7 @@ export class JetsonStrategy extends HTMLTemplateElement {
         hidden: area.hidden ?? false,
         order: area.order ?? Infinity,
         strategy: {
-          type: 'custom:ha-strategies',
+          type: 'custom:jetson-strategy',
           options: { area },
         },
       }))
@@ -235,76 +235,3 @@ console.info(
   'color: white; background: coral; font-weight: 700;',
   'color: coral; background: white; font-weight: 700;'
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import LightsView from "./views/LightsView";
-
-// interface LovelaceConfig {
-//   title?: string;
-//   views?: any[];
-//   [key: string]: any;
-// }
-
-// interface StrategyConfig {
-//   type: string;
-//   [key: string]: any;
-// }
-
-// export class HaStrategies extends HTMLElement {
-//   static async generate(config: StrategyConfig, hass: any): Promise<LovelaceConfig> {
-//     console.info('HA-STRATEGIES: Strategy options:', config);
-//     const generationStartTime = new Date();
-    
-//     const lightsCards = await LightsView.generate(hass);
-
-//     const generationEndTime = new Date();
-
-//     console.info('HA-STRATEGIES: Strategy generation took:', generationEndTime.getTime() - generationStartTime.getTime(), 'ms');
-//     return {
-//       title: "Lighting Dashboard",
-//       views: [
-//         {
-//           title: "Lights",
-//           path: "lights",
-//           cards: lightsCards
-//         }
-//       ]
-//     };
-//   }
-// }
-
-// // Register the strategy for Home Assistant to use
-// customElements.define('ll-strategy-dashboard-ha-strategies', HaStrategies);
